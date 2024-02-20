0fd9f50e0be575c66cf66d73a493ec6801e56b99
Seri <seri14dev@aura.ovo.run>
2024-02-21 05:34:09 +0900

    初回設定時の手順から構成を追加する操作を省略

--- src/addon-screenshot/dllmain.cpp
+++ src/addon-screenshot/dllmain.cpp
@@ -618,7 +618,7 @@ static void draw_setting_window(reshade::api::effect_runtime *runtime)
         }
         ImGui::TextUnformatted(_("Add set:"));
         ImGui::SameLine();
-        if (ImGui::Button(ICON_FK_PLUS))
+        if (ImGui::Button(ICON_FK_PLUS) || ctx.config.screenshot_mysets.empty())
         {
             const ini_file &config_file = ini_file::load_cache(ctx.environment.addon_screenshot_config_path);
             ctx.config.screenshot_mysets.emplace_back(config_file, "myset" + std::to_string(ctx.present_time.time_since_epoch().count()));
